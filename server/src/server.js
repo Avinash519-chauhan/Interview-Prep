93e70278a89b8290c64649f27d0f9ef35847769d
Avinash519-chauhan <ac9969160@gmail.com>
2026-08-04 19:57:38 +0530

    1st day

---
+++ server/src/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const express = require("express");
+const connectDB = require("./config/db")
+
+const app = express();
+
+app.use(express.json());
+connectDB();
+
+const PORT = process.env.PORT
+app.listen(PORT, (err) => err ? console.log(err) : console.log(`Server is Running on PORT ${PORT}`));
