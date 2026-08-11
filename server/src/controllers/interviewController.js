8b4b2f96364f7dbdda519439698821020b0d3b37
Avinash519-chauhan <ac9969160@gmail.com>
2026-08-11 17:52:50 +0530

    Repaire

--- server/src/controllers/interviewController.js
+++ server/src/controllers/interviewController.js
@@ -118,7 +118,7 @@ const getInterviewById = async (req, res) => {
 // Update Interview (Admin)
 const updateInterview = async (req, res) => {
   try {
-    let interviewId = req.prams.id;
+    let interviewId = req.params.id;
     if (!isValidObjectId(interviewId)) {
       return res.status(400).json({ msg: "Invalid Interview Id" });
     }
